afa620711352443aa7b1fef70d0e8a2a7ea0aa35
Herb Miller <herb@bobbingwide.com>
2015-09-03 12:33:56 +0100

    Add trace level functionality to bw_trace2() and bw_backtrace()
    New parameter for each API setting the required level of tracing for the
    trace record to be produced.
    Default is BW_TRACE_ALWAYS = 0
    Suggested value when actually running trace is to start with BW_TRACE_INFO
    and increase to BW_TRACE_DEBUG to see if that helps

--- libs/bwtrace.php
+++ libs/bwtrace.php
@@ -1,6 +1,6 @@
 <?php // (C) Copyright Bobbing Wide 2011-2015
 if ( !defined( "BWTRACE_INCLUDED" ) ) {
-define( "BWTRACE_INCLUDED", "2.0.1" );
+define( "BWTRACE_INCLUDED", "2.0.5" );
 define( "BWTRACE_FILE", __FILE__ );
 
 /**
@@ -13,11 +13,44 @@ define( "BWTRACE_FILE", __FILE__ );
  */
 
 /** 
+ * Constants for bw_trace2's $level parameter
+ *
+ * - The trace record is produced if the $level passed is greater than or equal to the current tracing level ( $bw_trace_on );
+ * - The default value for bw_trace2 is BW_TRACE_ALWAYS
+ * - The higher you set the value the more tracing you get.
+ * - The testing is NOT (yet) implemented as a bit-mask.
+ * - Note: These values are a subset of logging levels in packages such as monolog.
+ * - It's not really necessary to have CRITICAL, ALERT or EMERGENCY; ERROR will suffice
+ * - See also {@link https://en.wikipedia.org/wiki/Syslog#Severity_levels}
+ * 
+ */
+if ( !defined( 'BW_TRACE_DEBUG' ) ) { define( 'BW_TRACE_DEBUG', 32 ); }
+if ( !defined( 'BW_TRACE_INFO' ) ) { define( 'BW_TRACE_INFO', 16 ); }							// recommended level
+if ( !defined( 'BW_TRACE_NOTICE' ) ) { define( 'BW_TRACE_NOTICE', 8 ); }
+if ( !defined( 'BW_TRACE_WARNING' ) ) { define( 'BW_TRACE_WARNING', 4 ); }
+if ( !defined( 'BW_TRACE_ERROR' ) ) { define( 'BW_TRACE_ERROR', 2 ); }
+if ( !defined( 'BW_TRACE_ALWAYS' ) ) { define( 'BW_TRACE_ALWAYS', 0 ); }			// bw_trace2() default
+
+/** 
  * Assume tracing is off
  */
 if ( !isset( $bw_trace_on )) {
-  $bw_trace_on = false;
+	if ( defined( 'BW_TRACE_ON' ) ) {
+		$bw_trace_on = BW_TRACE_ON;
+	} else {
+		$bw_trace_on = false;
+	}
 }
+
+/**
+ * Only set trace level if BW_TRACE_LEVEL is defined and it's not already set
+ */
+if ( !isset( $bw_trace_level )) {
+	if ( defined( 'BW_TRACE_LEVEL' ) ) {
+		$bw_trace_level = BW_TRACE_LEVEL;
+	}
+}
+ 
 
 /**
  * Log a simple trace record to the trace log file if tracing is active
@@ -48,13 +81,14 @@ if ( !function_exists( "bw_trace" ) ) {
  * The value can be a simple field, array or complex object such as a post
  * @param string $text - additional information
  * @param bool $show_args - true if the parameter values are to be traced, false otherwise
+ * @param integer $level - the trace level requested
  * @return mixed $value - the first parameter
  */
 if ( !function_exists( "bw_trace2" ) ) { 
-	function bw_trace2( $value=NULL, $text=NULL, $show_args=true ) { 
-		global $bw_trace_on;
-		if ( $bw_trace_on ) { 
-			return( bw_lazy_trace2( $value, $text, $show_args ));
+	function bw_trace2( $value=NULL, $text=NULL, $show_args=true, $level=BW_TRACE_ALWAYS ) { 
+		global $bw_trace_on, $bw_trace_level;
+		if ( $bw_trace_on && ( $level <= $bw_trace_level ) ) { 
+			return( bw_lazy_trace2( $value, $text, $show_args, $level ));
 		} else {  
 			return( $value );
 		}
@@ -66,9 +100,9 @@ if ( !function_exists( "bw_trace2" ) ) {
  * 
  */
 if ( !function_exists( "bw_backtrace" ) ) { 
-	function bw_backtrace() {
-		global $bw_trace_on;
-		if ( $bw_trace_on ) {
+	function bw_backtrace( $level=BW_TRACE_ALWAYS ) {
+		global $bw_trace_on, $bw_trace_level;
+		if ( $bw_trace_on && ( $level <= $bw_trace_level ) ) { 
 			bw_lazy_backtrace();
 		}
 	}
