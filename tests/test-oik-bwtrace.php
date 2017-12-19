193b8db86007bbae4d56e7f5596f76df99f3c587
Herb Miller <herb@bobbingwide.com>
2017-12-19 11:28:19 +0000

    Issue #55 - add tests for bw_trace_plugin_startup - trace off and on

--- tests/test-oik-bwtrace.php
+++ tests/test-oik-bwtrace.php
@@ -40,7 +40,13 @@ class Tests_oik_bwtrace extends BW_UnitTestCase {
 	 */
 	function save_bw_trace_options() {
 		global $bw_trace_options;
+		//print_r( $bw_trace_options );
 		$this->bw_trace_options = $bw_trace_options;
+	}
+	
+	function save_bw_action_options() {
+		global $bw_action_options;
+		$this->bw_action_options = $bw_action_options;
 	}
 	
 	/**
@@ -50,6 +56,24 @@ class Tests_oik_bwtrace extends BW_UnitTestCase {
 		global $bw_trace_options;
 		$bw_trace_options = $this->bw_trace_options;
 	}
+	
+	/**
+	 * Restore the saved action options
+	 */
+	function restore_bw_action_options() {
+		global $bw_action_options;
+		$bw_action_options = $this->bw_action_options;
+	}
+	
+	/**
+	 * Update bw_trace_options
+	 */
+	function update_bw_trace_options() {
+		global $bw_trace_options;
+		update_option( "bw_trace_options", $bw_trace_options );
+		//print_r( $bw_trace_options );
+	}
+	
 	
 	/**
 	 * Set the trace options to defined values
@@ -78,11 +102,43 @@ class Tests_oik_bwtrace extends BW_UnitTestCase {
 	function init_bw_trace_options() {
 		global $bw_trace_options;
 		//print_r( $bw_trace_options );
-		$bw_trace_options['trace_ajax'] = 'on'; 
-		$bw_trace_options['trace'] = '0';
+		$bw_trace_options['file'] = 'bwphpunit.loh';
 		$bw_trace_options['reset'] = 'on';
+		$bw_trace_options['trace'] = '0';
+		$bw_trace_options['file_ajax'] = 'bwphpunit.ajax';
 		$bw_trace_options['reset_ajax'] = 'on';
+		$bw_trace_options['trace_ajax'] = 'on'; 
 		$bw_trace_options['level'] = BW_TRACE_INFO; // 16
+		
+    $bw_trace_options['qualified'] = 'on';
+    $bw_trace_options['count'] = 'on';
+    $bw_trace_options['date'] = 'on';
+    $bw_trace_options['filters'] = 'on';
+    $bw_trace_options['num_queries'] = 'on';
+    $bw_trace_options['post_id'] = 'on';
+    $bw_trace_options['memory'] = 'on';
+    $bw_trace_options['files'] = 'on';
+    $bw_trace_options['ip'] = php_sapi_name();
+	}
+	
+	function init_bw_action_options() {
+		global $bw_action_options;
+		$bw_action_options['count'] = 'on';
+    $bw_action_options['trace_deprecated'] = 'on';
+    $bw_action_options['trace_errors'] = 'on';
+    $bw_action_options['trace_wp_action'] = '0';
+    $bw_action_options['trace_wp_rewrite'] = '0';
+    $bw_action_options['trace_included_files'] = 'on';
+    $bw_action_options['trace_saved_queries'] = 'on';
+    $bw_action_options['trace_output_buffer'] = 'on';
+    $bw_action_options['trace_functions'] = 'on';
+    $bw_action_options['trace_status_report'] = 'on';
+    $bw_action_options['hooks'] = '';
+    $bw_action_options['results'] = '';
+    $bw_action_options['post_hooks'] = '';
+    $bw_action_options['hook_funcs'] = '';
+    $bw_action_options['backtrace'] = '';
+    $bw_action_options['stringwatch'] = '';
 	} 
 	
 	/**
@@ -145,6 +201,57 @@ class Tests_oik_bwtrace extends BW_UnitTestCase {
 		$this->assertFalse( $trace );
 		$this->restore_bw_trace_options();
 	}
+	
+	/**
+	 * Test trace plugin startup with tracing off
+	 */
+	function test_bw_trace_plugin_startup_tracing_off() {
+		global $bw_trace_options;
+		global $bw_action_options;
+	
+		$this->save_bw_trace_options(); 
+		$this->init_bw_trace_options();
+		$this->update_bw_trace_options();
+		$this->save_bw_action_options();
+		$this->init_bw_action_options();
+		
+		bw_trace_plugin_startup();
+		
+		$this->restore_bw_trace_options();
+		$this->restore_bw_action_options();
+		
+		$tracing = bw_trace_status();
+		$this->assertFalse( $tracing );
+	
+	}
+	
+	/**
+	 * Test trace plugin startup with tracing on
+	 * 
+	 * - We have to ensure tracing is on and to a file we control
+	 * - The trace file should be reset.
+	 */
+	function test_bw_trace_plugin_startup_tracing_on() {
+		global $bw_trace_options;
+		global $bw_action_options;
+	
+		$this->save_bw_trace_options(); 
+		$this->init_bw_trace_options();
+		$bw_trace_options['trace'] = 'on';
+		$this->update_bw_trace_options();
+		
+		$this->save_bw_action_options();
+		$this->init_bw_action_options();
+		
+		bw_trace_plugin_startup();
+		
+		$tracing = bw_trace_status();
+		$this->assertTrue( $tracing );
+		
+		$this->restore_bw_trace_options();
+		$this->restore_bw_action_options();
+	}
+	
 		
 	
 		
