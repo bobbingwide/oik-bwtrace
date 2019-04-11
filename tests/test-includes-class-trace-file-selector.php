fb455dbb320fbc1eb98b7e384ec0f20aa0f2478f
Herb Miller <herb@bobbingwide.com>
2019-04-11 13:24:51 +0100

    Issue #16 - correct class trace_file_selector tests

--- tests/test-includes-class-trace-file-selector.php
+++ tests/test-includes-class-trace-file-selector.php
@@ -10,12 +10,26 @@ class Tests_includes_class_trace_file_selector extends BW_UnitTestCase {
 	
 	function test__construct() {
 	
-		$trace_file_selector = new trace_file_selector();
+
+
+		$trace_file_selector = $this->get_trace_file_selector();
+
 		$this->assertInstanceOf( "trace_file_selector", $trace_file_selector);
 		$filename = $trace_file_selector->get_trace_file_mask();
-		$this->assertEquals( ABSPATH . "bwtrace.loh" , $filename );
+		$this->assertEquals( __DIR__ . "/data/bwtrace.loh" , $filename );
 		//$trace_file_name = $trace_file_selector->get_trace_file_name();
 		//$this->assertEquals( ABSPATH . "bwtrace.loh.3", $trace_file_name );
+	}
+
+	function get_trace_file_selector() {
+		$trace_files_directory = new trace_files_directory();
+		$trace_files_directory->set_fq_trace_files_directory( __DIR__ . '/data');
+		$trace_file_selector = new trace_file_selector();
+		$trace_file_selector->set_trace_files_directory( $trace_files_directory );
+		$trace_file_selector->set_limit( 10 );
+		return $trace_file_selector;
+
+
 	}
 	
 	/** 
@@ -71,8 +85,10 @@ class Tests_includes_class_trace_file_selector extends BW_UnitTestCase {
 	 * 
 	 */
 	function test_find_next_unused() {
-		$trace_file_selector = new trace_file_selector();
-		$trace_file_selector->set_file_path( dirname( __FILE__ ) . '/data/' );
+		$trace_file_selector = $this->get_trace_file_selector();
+		//$trace_file_selector->set_file_path( dirname( __FILE__ ) . '/data/' );
+		$trace_file_selector->set_file_name( "bwtrace");
+		$trace_file_selector->set_file_extension( "loh");
 		$next = $trace_file_selector->query_next_generation();
 		
 		$this->assertEquals( 3, $next );
@@ -84,8 +100,10 @@ class Tests_includes_class_trace_file_selector extends BW_UnitTestCase {
 	 * What if the oldest file is outwith the limit?
 	 */ 
 	function test_query_oldest_file() {
-		$trace_file_selector = new trace_file_selector();
-		$trace_file_selector->set_file_path( dirname( __FILE__ ) . '/data/' );
+		$trace_file_selector = $this->get_trace_file_selector();
+		//$trace_file_selector->set_file_path( dirname( __FILE__ ) . '/data/' );
+		$trace_file_selector->set_file_name( "bwtrace");
+		$trace_file_selector->set_file_extension( "loh");
 		$trace_file_selector->set_limit( 2 );
 		$next = $trace_file_selector->query_next_generation();
 		$this->assertEquals( 1, $next );
