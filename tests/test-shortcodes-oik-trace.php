5c9ee714c511fbf3a8782808fe1c3c069abd6e59
Herb Miller <herb@bobbingwide.com>
2019-04-11 18:30:21 +0100

    Issue #69 - adjust tests for bwtrace shortcode

--- tests/test-shortcodes-oik-trace.php
+++ tests/test-shortcodes-oik-trace.php
@@ -1,4 +1,4 @@
-<?php // (C) Copyright Bobbing Wide 2017
+<?php // (C) Copyright Bobbing Wide 2017-2019
 
 /**
  * @package 
@@ -11,7 +11,7 @@ class Tests_shortcodes_oik_trace extends BW_UnitTestCase {
 		parent::setUp();
 		
 		oik_require_lib( "oik-sc-help" );
-		oik_require( "shortcodes/oik-trace.php" ); 	
+		oik_require( "shortcodes/oik-trace.php", "oik-bwtrace" );
 		oik_require_lib( "oik_plugins" );
 	}
 	
@@ -38,7 +38,7 @@ class Tests_shortcodes_oik_trace extends BW_UnitTestCase {
 	 * - We expect trace to be enabled
 	 */ 
 	function test_bwtrace__example() {
-		bw_update_option( "file", "bwtraces.loh", "bw_trace_options" );
+		//bw_update_option( "file", "bwtraces.loh", "bw_trace_options" );
 		$this->switch_to_locale( "en_GB" );
 		$html = bw_ret( bwtrace__example() );
 		$html = $this->replace_home_url( $html );
@@ -48,13 +48,30 @@ class Tests_shortcodes_oik_trace extends BW_UnitTestCase {
 	}
 	
 	function test_bwtrace__example_bb_BB() {
-		bw_update_option( "file", "bwtraces.loh", "bw_trace_options" );
+		//bw_update_option( "file", "bwtraces.loh", "bw_trace_options" );
 		$this->switch_to_locale( "bb_BB" );
 		$html = bw_ret( bwtrace__example() );
 		$html = $this->replace_home_url( $html );
 		//$this->generate_expected_file( $html );
 		$this->assertArrayEqualsFile( $html );
 		$this->switch_to_locale( "en_GB" );
+	}
+
+	/**
+	 * Reloads the text domains
+	 *
+	 * - Loading the 'oik-libs' text domain from the oik-libs plugin invalidates tests where the plugin is delivered from WordPress.org so oik-libs won't exist.
+	 * - but we do need to reload (oik's and?) oik-bwtrace's text domains
+	 * - and cause the null domain to be rebuilt.
+	 */
+	function reload_domains() {
+		$domains = array( "oik", "oik-bwtrace" );
+		foreach ( $domains as $domain ) {
+			$loaded = bw_load_plugin_textdomain( $domain );
+			$this->assertTrue( $loaded, "$domain not loaded" );
+		}
+		oik_require_lib( "oik-l10n" );
+		oik_l10n_enable_jti();
 	}
 	
 	
