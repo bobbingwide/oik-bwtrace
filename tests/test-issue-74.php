f178fd535e4e0283fc09349f4de98db821719e09
Herb Miller <herb@bobbingwide.com>
2019-11-21 21:25:50 +0000

    Tests #74 - attempt to test the reason for using bw_trace_obsafe_print_r

--- tests/test-issue-74.php
+++ tests/test-issue-74.php
@@ -39,10 +39,9 @@ class Tests_issue_74 extends BW_UnitTestCase {
 	 * When the return parameter is used, this function uses internal output buffering
 	 * so it cannot be used inside an ob_start() callback function.
 	 *
-	 * OK. so how come we can call print_r safely against the array.
+	 * OK then. How come we can call print_r safely against the array?
 	 * Is it because we're not inside an ob_start() callback function?
 	 */
-
 	function test_bw_trace_print_r_obsafe() {
 		ob_start();
 		$handlers = ob_list_handlers();
@@ -63,8 +62,33 @@ class Tests_issue_74 extends BW_UnitTestCase {
 		//$print_r = print_r( $tests, true );
 		$this->assertEquals( $expected, $output );
 		ob_end_flush();
-
 	}
 
+	/**
+	 * Attempts to do something that shouldn't be allowed in an output handler callback function
+	 * Seems that PHP 7.x doesn't allow you to call print_r inside a callback handler.
+	 * See the docs https://www.php.net/manual/en/function.ob-start.php
+	 *
+	 * @param $text
+	 * @return mixed|string
+	 */
+	function print_r_buffer_handler( $text ) {
+		$array = array( "norty");
+		$norty = print_r( $array, true );
+		$norty .= print_r( $this->print_r_buffer_handler, true );
+		$norty = __FUNCTION__;
+		return $norty;
+	}
+
+	function test_obstart_print_r_problem() {
+		$handlers = ob_list_handlers();
+		ob_start( [$this, 'print_r_buffer_handler'] );
+		$handlers = ob_list_handlers();
+		$expected = [ 'default output handler', 'Tests_issue_74::print_r_buffer_handler'];
+		$this->assertEquals( $expected, $handlers );
+		echo 'fred';
+		ob_end_flush();
+		$this->expectOutputString( 'print_r_buffer_handler' );
+	}
 
 }
